306c3d271e603068d1115717f76e9a2d14e1b04d
Roman Kuhta <romakuhta@mail.com>
2023-08-12 14:29:34 +0300

    feat: ThreadPoolTask

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -6,11 +6,24 @@ public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+
+        // Invert FIFO behavior to LIFO
+        LinkedBlockingDeque<Runnable> workQueue = new LinkedBlockingDeque<>() {
+            @Override
+            public Runnable take() throws InterruptedException {
+                return super.takeLast();
+            }
+        };
+
+        return new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                workQueue);
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8,
+                0L, TimeUnit.MILLISECONDS,
+                new SynchronousQueue<Runnable>(), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
     }
 }
